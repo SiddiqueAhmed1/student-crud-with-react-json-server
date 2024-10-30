fc13a9dde581e6294775d4bce8faae9ed22a911e
Siddique Ahmed <developersiddique123@gmail.com>
2024-10-30 17:34:41 +0600

    edit modal add done

--- src/components/ReactBoostrap.jsx
+++ src/components/ReactBoostrap.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -10,6 +10,7 @@ import {
   Row,
   Table,
 } from "react-bootstrap";
+import { FaEye, FaRegEdit, FaTrash } from "react-icons/fa";
 import Swal from "sweetalert2";
 
 const ReactBoostrap = () => {
@@ -27,7 +28,10 @@ const ReactBoostrap = () => {
   // get all students state
   const [students, setStudents] = useState([]);
 
-  // form value taken
+  // student edit state
+  const [editModalShow, setEditModalShow] = useState(false);
+
+  //student form value taken
   const handleInputValue = (e) => {
     setInput((prevState) => ({
       ...prevState,
@@ -39,6 +43,7 @@ const ReactBoostrap = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
+    // form validation
     if (!input.name || !input.age || !input.roll || !input.photo) {
       Swal.fire({
         icon: "error",
@@ -52,15 +57,19 @@ const ReactBoostrap = () => {
         icon: "success",
         title: "Student has been saved successfuly",
         showConfirmButton: false,
-        timer: 1500,
+        timer: 2000,
+      });
+      setTimeout(() => {
+        handleModalHide();
+        getAllStudents();
+      }, 1000);
+      setInput({
+        name: "",
+        age: "",
+        roll: "",
+        photo: "",
       });
     }
-    setInput({
-      name: "",
-      age: "",
-      roll: "",
-      photo: "",
-    });
   };
 
   // get all students
@@ -69,23 +78,47 @@ const ReactBoostrap = () => {
 
     setStudents(allStudents.data);
   };
-  getAllStudents()
 
-  // modal showing function
+  //student add modal show
   const handleModalShow = () => {
     setModal(true);
   };
 
-  // modal hide function
-  const handleInputHide = () => {
+  //student add modal hide
+  const handleModalHide = () => {
     setModal(false);
   };
+
+  // student delete
+  const handleStudentDelete = async (id) => {
+    await axios.delete(`http://localhost:7070/students/${id}`);
+
+    getAllStudents();
+  };
+  // student edit modal show
+  const handleEditModal = (id) => {
+     setInput(students.find((data) => data.id === id))
+
+    setEditModalShow(true);
+  };
+
+  // student edit modal hide
+  const handleEditModalHide = () => {
+    setEditModalShow(false)
+  }
+
+
+
+  // protect student reRender
+  useEffect(() => {
+    getAllStudents();
+  }, []);
 
   return (
     <>
       <Container className="my-5">
         <Row className="justify-content-center">
-          <Col xl={10}>
+          <Col xl={10} sm={10}>
             <Button
               onClick={handleModalShow}
               className="mb-2"
@@ -99,6 +132,7 @@ const ReactBoostrap = () => {
               </Card.Header>
               <Card.Body>
                 <Table>
+                  {/* student table header */}
                   <thead>
                     <tr>
                       <th>#</th>
@@ -110,39 +144,51 @@ const ReactBoostrap = () => {
                     </tr>
                   </thead>
                   <tbody className="align-middle">
-                    {students.map((item, index) => {
-                          return (
-                            <tr>
-                              <td>{index + 1}</td>
-                              <td>
-                                <img
-                                  style={{
-                                    width: "50px",
-                                    height: "50px",
-                                    objectFit: "cover",
-                                    borderRadius: "50%",
-                                  }}
-                                  src={item.photo}
-                                  alt=""
-                                />
-                              </td>
-                              <td>{item.name}</td>
-                              <td>{item.roll}</td>
-                              <td>{item.age}</td>
-                              <td>
-                                <Button className="me-2" variant="info">
-                                  Edit
-                                </Button>
-                                <Button className="me-2" variant="warning">
-                                  View
-                                </Button>
-                                <Button className="me-2" variant="danger">
-                                  Delete
-                                </Button>
-                              </td>
-                            </tr>
-                          );
-                        })}
+                    {students.length === 0 ? (
+                      <tr>No Data Found</tr>
+                    ) : (
+                      students.map((item, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>
+                              <img
+                                style={{
+                                  width: "50px",
+                                  height: "50px",
+                                  objectFit: "cover",
+                                  borderRadius: "50%",
+                                }}
+                                src={item.photo}
+                                alt=""
+                              />
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.roll}</td>
+                            <td>{item.age}</td>
+                            <td>
+                              <Button
+                                onClick={() => handleEditModal(item.id)}
+                                className="me-2"
+                                variant="info"
+                              >
+                                <FaRegEdit />
+                              </Button>
+                              <Button className="me-2" variant="warning">
+                                <FaEye />
+                              </Button>
+                              <Button
+                                onClick={() => handleStudentDelete(item.id)}
+                                className="me-2"
+                                variant="danger"
+                              >
+                                <FaTrash />
+                              </Button>
+                            </td>
+                          </tr>
+                        );
+                      })
+                    )}
                   </tbody>
                 </Table>
               </Card.Body>
@@ -150,12 +196,56 @@ const ReactBoostrap = () => {
           </Col>
         </Row>
       </Container>
-      <Modal show={modal} onHide={handleInputHide} centered>
+      {/* student add modal with form */}
+      <Modal show={modal} onHide={handleModalHide} centered>
         <Modal.Header>
           <Modal.Title>Add New Student</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <form onSubmit={handleFormSubmit}>
+            <label htmlFor="">Name</label>
+            <input
+              name="name"
+              type="text"
+              className="form-control mb-2"
+              onChange={handleInputValue}
+              value={input.name}
+            />
+            <label htmlFor="">Roll</label>
+            <input
+              name="roll"
+              type="text"
+              className="form-control mb-2"
+              onChange={handleInputValue}
+              value={input.roll}
+            />
+            <label htmlFor="">Age</label>
+            <input
+              name="age"
+              type="text"
+              className="form-control mb-2"
+              onChange={handleInputValue}
+              value={input.age}
+            />
+            <label htmlFor="">Photo</label>
+            <input
+              name="photo"
+              type="text"
+              className="form-control mb-2"
+              onChange={handleInputValue}
+              value={input.photo}
+            />
+            <Button type="submit">Add</Button>
+          </form>
+        </Modal.Body>
+      </Modal>
+      {/* student edit modal */}
+      <Modal show={editModalShow} onHide={handleEditModalHide} centered>
+        <Modal.Header>
+          <Modal.Title>Edit Student</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
             <label htmlFor="">Name</label>
             <input
               name="name"
